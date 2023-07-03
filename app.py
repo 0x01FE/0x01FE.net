ee62efa48776995adaa4c461ba0809e603c919ba
Jackson H <jackson.d.hodge@jbhunt.com>
2023-07-03 16:30:18 -0500

    more tables for music stats

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, current_app, render_template, redirect
+from os.path import exists
+from math import floor
 import json
 
 
@@ -14,8 +16,13 @@ assets_path = "~/0x01fe.net/assets/"
 
 
 
-def militohours(mili : int) -> int:
-    return mili/1000/60/60
+def listenTimeFormat(mili : int) -> tuple:
+
+    minutes = mili/1000/60
+    hours = floor(minutes/60)
+
+    return (hours, round(minutes % 60))
+
 
 
 def keyfunc(tup :  tuple) -> int:
@@ -51,12 +58,13 @@ def media():
     data = sorted(data.items(), key=keyfunc, reverse=True)
 
     top_ten = {}
-    i = 1
+    i = 0
     for artist_tuple in data:
         artist_name, artist_info = artist_tuple
-        top_ten[artist_name.replace("-", " ")] = round(militohours(artist_info["overall"]), 1) # miliseconds to hours
+
+        top_ten[artist_name.replace("-", " ")] =  listenTimeFormat(artist_info["overall"])
         i+=1
-        if i == 11:
+        if i == 10:
             break
 
     return render_template('media.html', css_path=css_path, assets_path=assets_path, data=top_ten)
@@ -74,12 +82,47 @@ def contact():
     return render_template('contact.html', css_path=css_path, assets_path=assets_path)
 
 
+
 @app.route('/music/<artist>')
 def music(artist : str):
+
+    artist = artist.lower()
 
     with open(spotify_times_path + "overall.json", 'r') as f:
         data = json.loads(f.read())
 
-    artist_listen_time = round(militohours(data[artist]["overall"]), 1)
+    listen_time = listenTimeFormat(data[artist]["overall"])
 
-    return render_template(f'./music/{artist}.html', css_path=css_path, assets_path=assets_path, artist_listen_time=artist_listen_time)
+    # Make a dict of my top five albums for artist
+    albums_sorted = sorted(data[artist]["albums"].items(), key=keyfunc, reverse=True)
+
+    top_albums = {}
+    i = 0
+    for album_tuple in albums_sorted:
+        album_name, album_info = album_tuple
+
+        top_albums[album_name] = listenTimeFormat(album_info["overall"])
+        i+=1
+        if i == 5:
+            break
+
+    # Make a dict of my top ten songs for artist
+    all_songs = {}
+    for album in data[artist]["albums"]:
+        for song in data[artist]["albums"][album]["songs"]:
+            all_songs[song] = data[artist]["albums"][album]["songs"][song]
+
+    sorted_songs = {k: v for k, v in sorted(all_songs.items(), key=lambda item: item[1], reverse=True)}
+
+    top_songs = {}
+    i = 0
+    for song in sorted_songs:
+        top_songs[song] = listenTimeFormat(sorted_songs[song])
+        i+=1
+        if i == 10:
+            break
+
+    if exists(f'./music{artist}.html'):
+        return render_template(f'./music/{artist}.html', css_path=css_path, assets_path=assets_path, artist_listen_time=listen_time, top_albums=top_albums, top_songs=top_songs)
+    else:
+        return render_template('./music/default-artist.html', css_path=css_path, assets_path=assets_path, artist_name=artist.title() , artist_listen_time=listen_time, top_albums=top_albums, top_songs=top_songs)
